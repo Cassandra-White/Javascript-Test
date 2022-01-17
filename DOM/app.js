577dc0152e91f39d13f394cd4dae034b0c5418f3
Cassandra-White <kriss.alex78@gmail.com>
2022-01-17 12:10:13 +0100

    simple review de gestion du dom

--- DOM/app.js
+++ DOM/app.js
@@ -1,4 +1,5 @@
 const allLis = document.querySelectorAll('li')
+const colors = ['black', 'purple','blue', 'red', 'green', 'grey']
 
 for(let i = 0; i < allLis.length; i++){
   allLis[i].innerText = 'New Content'
@@ -7,3 +8,7 @@ for(let i = 0; i < allLis.length; i++){
 for(let li of allLis){
   li.innerHTML = '<b>Cette fois en Gras</b>'
 }
+
+allLis.forEach((li, index) =>{
+  li.style.color = colors[index]
+})
